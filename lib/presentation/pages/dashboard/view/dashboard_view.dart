70741a0d48408e301fe88681f1d2de5e9fbe4037
blockchainstar12 <blockchainstar18@gmail.com>
2024-07-20 13:21:12 +0000

    history

--- lib/presentation/pages/dashboard/view/dashboard_view.dart
+++ lib/presentation/pages/dashboard/view/dashboard_view.dart
@@ -41,6 +41,7 @@ class _DashboardScreenState extends State<DashboardScreen>
             DebitCardRoute(),
             HelpCenterRoute(),
             HistoryRoute(),
+            //history section
           ],
           //  transitionBuilder:_customTransitionBuilder ,
           builder: (context, child) {
